8f5978d049becd3d569972514e013b88651de138
prakash krishnan <prakash.krishnan1985@gmail.com>
2016-02-10 14:08:37 +0530

    initial commit

---
+++ src/test/java/com/eye/EyesUtil.java
@@ -0,0 +1,11 @@
+package com.eye;
+import com.applitools.eyes.Eyes;
+import com.applitools.eyes.RectangleSize;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.net.URISyntaxException;
+
+public class EyesUtil {
+
+}
